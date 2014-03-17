2c83e85e139df0d113815de8ab8c51b2c1b0ecf2
Daniel Lo Nigro <daniel@dan.cx>
2014-03-16 17:13:44 -0700

    Add build script

--- src/SharedAssemblyVersionInfo.cs
+++
@@ -1,3 +0,0 @@
-[assembly: System.Reflection.AssemblyVersion("1.0")]
-[assembly: System.Reflection.AssemblyFileVersion("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersion("1.0.0.0")]
