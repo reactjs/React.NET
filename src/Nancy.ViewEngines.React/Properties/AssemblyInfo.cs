783038ce6df4129d17834077e151c49be1a6c054
prabirshrestha <mail@prabir.me>
2014-04-08 10:36:59 -0400

    updated assemblyinfo and added shared files to Nancy.ViewEngines.React

--- src/Nancy.ViewEngines.React/Properties/AssemblyInfo.cs
+++ src/Nancy.ViewEngines.React/Properties/AssemblyInfo.cs
@@ -1,36 +1,7 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Nancy.ViewEngines.React")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Nancy.ViewEngines.React")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+[assembly: AssemblyDescription("ReactJS.NET View Engine for Nancy")]
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("caf09580-7c61-4282-acb0-582c158952f7")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: Guid("e728cdeb-9ed2-4a8f-8873-9247580db50b")]
