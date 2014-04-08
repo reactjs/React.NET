17f3ab920907e9ce05b23c3fc651419794a9faba
prabirshrestha <mail@prabir.me>
2014-04-08 01:21:24 -0400

    added react sample

---
+++ src/React.Sample.Nancy/views/hi.jsx
@@ -0,0 +1,16 @@
+﻿/** @jsx React.DOM */
+
+var Hi = React.createClass({
+	render: function () {
+		return (
+			<html>
+				<head></head>
+				<body>
+					<h1>Hi {this.props.name}</h1>
+				</body>
+			</html>
+		)
+	}
+});
+
+module.exports = Hi;
