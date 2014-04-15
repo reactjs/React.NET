c32cd869796570c411a4f2366fb38613695e3f43
Daniel Lo Nigro <daniel@dan.cx>
2014-04-14 22:14:10 -0700

    NuGet package for MSBuild task

---
+++ src/React.MSBuild/tools/install.ps1
@@ -0,0 +1,37 @@
+﻿function Resolve-ProjectName {
+    param(
+        [parameter(ValueFromPipelineByPropertyName = $true)]
+        [string[]]$ProjectName
+    )
+    
+    if($ProjectName) {
+        $projects = Get-Project $ProjectName
+    }
+    else {
+        # All projects by default
+        $projects = Get-Project
+    }
+    
+    $projects
+}
+
+function Get-MSBuildProject {
+    param(
+        [parameter(ValueFromPipelineByPropertyName = $true)]
+        [string[]]$ProjectName
+    )
+    Process {
+        (Resolve-ProjectName $ProjectName) | % {
+            $path = $_.FullName
+            @([Microsoft.Build.Evaluation.ProjectCollection]::GlobalProjectCollection.GetLoadedProjects($path))[0]
+        }
+    }
+}
+
+$project = Get-Project
+$buildProject = Get-MSBuildProject
+$target = $buildProject.Xml.AddTarget("TransformJsx")
+$target.AfterTargets = "Build"
+$task = $target.AddTask("Exec")
+$task.SetParameter("Command", '"$(msbuildtoolspath)\msbuild.exe" $(ProjectDirectory)TransformJsx.proj /p:OutputPath=$(OutputPath) /nr:false')
+$project.Save()
---
+++ src/React.MSBuild/tools/uninstall.ps1
@@ -0,0 +1,39 @@
+﻿function Resolve-ProjectName {
+    param(
+        [parameter(ValueFromPipelineByPropertyName = $true)]
+        [string[]]$ProjectName
+    )
+    
+    if($ProjectName) {
+        $projects = Get-Project $ProjectName
+    }
+    else {
+        # All projects by default
+        $projects = Get-Project
+    }
+    
+    $projects
+}
+
+function Get-MSBuildProject {
+    param(
+        [parameter(ValueFromPipelineByPropertyName = $true)]
+        [string[]]$ProjectName
+    )
+    Process {
+        (Resolve-ProjectName $ProjectName) | % {
+            $path = $_.FullName
+            @([Microsoft.Build.Evaluation.ProjectCollection]::GlobalProjectCollection.GetLoadedProjects($path))[0]
+        }
+    }
+}
+
+$project = Get-Project
+$buildProject = Get-MSBuildProject
+
+$existingImports = $buildProject.Xml.Targets | where {$_.Name -eq "TransformJsx"}
+foreach ($import in $existingImports) {
+	$buildProject.Xml.RemoveChild($import)
+}
+
+$project.Save()
