33c230f0ac02d5ba4b5d4945fe0ff1d67465550c
Daniel Lo Nigro <daniel@dan.cx>
2014-04-20 17:11:10 -0700

    Add assembly registration for VroomJs. Throw exception if error occurs while initialising on a supported environment (Linux or Mac)

---
+++ src/React.JavaScriptEngine.VroomJs/VroomJsInitialisationException.cs
@@ -0,0 +1,47 @@
+﻿/*
+ *  Copyright (c) 2014, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant 
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+using System;
+using System.Runtime.Serialization;
+using React.Exceptions;
+
+namespace React.JavaScriptEngine.VroomJs
+{
+	/// <summary>
+	/// Thrown when the JavaScript engine does not support JSX transformation
+	/// </summary>
+	[Serializable]
+	public class VroomJsInitialisationException : ReactException
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="VroomJsInitialisationException"/> class.
+		/// </summary>
+		public VroomJsInitialisationException(string innerMessage) : 
+			base(GetMessage(innerMessage)) { }
+
+		/// <summary>
+		/// Used by deserialization
+		/// </summary>
+		protected VroomJsInitialisationException(SerializationInfo info, StreamingContext context)
+			: base(info, context) { }
+
+		/// <summary>
+		/// Gets a message that describes the current exception.
+		/// </summary>
+		private static string GetMessage(string innerMessage)
+		{
+			return
+				"Failed to initialise VroomJs. This is most likely caused by the native library " +
+				"(libvroomjs.so) being out of date or your system lacking a compatible version of " +
+				"V8. Please run Mono with the `MONO_LOG_LEVEL=debug` environment variable for " +
+				"more debugging information. \n\n " +
+				"More details: " + innerMessage;
+		}
+	}
+}
---
+++ src/React.JavaScriptEngine.VroomJs/VroomJsUtils.cs
@@ -0,0 +1,54 @@
+﻿/*
+ *  Copyright (c) 2014, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant 
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+using System;
+using React.Exceptions;
+
+namespace React.JavaScriptEngine.VroomJs
+{
+	/// <summary>
+	/// Utility methods for VroomJs JavaScript engine
+	/// </summary>
+	public static class VroomJsUtils
+	{
+		/// <summary>
+		/// Determines if the current environment supports the VroomJs engine
+		/// </summary>
+		/// <returns></returns>
+		public static bool IsEnvironmentSupported()
+		{
+			return Environment.OSVersion.Platform == PlatformID.Unix;
+		}
+
+		/// <summary>
+		/// If the user is explicitly referencing this assembly, they probably want to use it.
+		/// Attempt to use the engine and throw an exception if it doesn't work.
+		/// </summary>
+		public static void EnsureEngineFunctional()
+		{
+			int result = 0;
+			try
+			{
+				using (var engine = new VroomJsEngine())
+				{
+					result = engine.Evaluate<int>("1 + 1");
+				}
+			}
+			catch (Exception ex)
+			{
+				throw new VroomJsInitialisationException(ex.Message);
+			}
+
+			if (result != 2)
+			{
+				throw new ReactException("Mathematics is broken. 1 + 1 = " + result);
+			}
+		}
+	}
+}
