1072de82570713d66c66c99ff9f35b961e45990d
Marcin Drobik <mdrobik@future-processing.com>
2015-01-11 15:58:38 +0100

    Cleaning/fixing program.cs

--- src/React.Sample.Owin/Program.cs
+++ src/React.Sample.Owin/Program.cs
@@ -9,14 +9,8 @@
 
 using System;
 
-using Microsoft.Owin.FileSystems;
 using Microsoft.Owin.Hosting;
-using Microsoft.Owin.StaticFiles;
 using Microsoft.Owin.StaticFiles.Infrastructure;
-
-using Owin;
-
-using React.Owin;
 
 namespace React.Sample.Owin
 {
@@ -30,37 +24,4 @@ namespace React.Sample.Owin
             }
         }
     }
-
-    public class Startup
-    {
-        public void Configuration(IAppBuilder app)
-        {
-#if DEBUG
-            app.UseErrorPage();
-#endif
-
-            app.Use(
-                async (context, next) =>
-                {
-                    Console.WriteLine("{0} {1} {2}", context.Request.Method, context.Request.Path, context.Request.QueryString);
-
-                    try
-                    {
-                        await next();
-                    }
-                    catch (Exception exception)
-                    {
-                        Console.WriteLine(exception.ToString());
-                        throw;
-                    }
-                });
-
-            var contentFileSystem = new PhysicalFileSystem("Content");
-
-            app.UseJsxFiles(new JsxFileOptions() { StaticFileOptions = new StaticFileOptions() { FileSystem = contentFileSystem }});
-            app.UseFileServer(new FileServerOptions() { FileSystem = contentFileSystem });
-        }
-    }
-
-    
 }
