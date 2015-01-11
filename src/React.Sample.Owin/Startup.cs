82e843017a3f824e3669f71d3a633d051263600d
Marcin Drobik <mdrobik@future-processing.com>
2015-01-11 15:57:51 +0100

    Cleaning program.cs

--- src/React.Sample.Owin/Startup.cs
+++ src/React.Sample.Owin/Startup.cs
@@ -1,18 +1,7 @@
-﻿/*
- *  Copyright (c) 2014-2015, Facebook, Inc.
- *  All rights reserved.
- *
- *  This source code is licensed under the BSD-style license found in the
- *  LICENSE file in the root directory of this source tree. An additional grant 
- *  of patent rights can be found in the PATENTS file in the same directory.
- */
-
-using System;
+﻿using System;
 
 using Microsoft.Owin.FileSystems;
-using Microsoft.Owin.Hosting;
 using Microsoft.Owin.StaticFiles;
-using Microsoft.Owin.StaticFiles.Infrastructure;
 
 using Owin;
 
@@ -20,17 +9,6 @@ using React.Owin;
 
 namespace React.Sample.Owin
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            using (WebApp.Start<Startup>("http://localhost:12345"))
-            {
-                Console.ReadLine();
-            }
-        }
-    }
-
     public class Startup
     {
         public void Configuration(IAppBuilder app)
@@ -61,6 +39,4 @@ namespace React.Sample.Owin
             app.UseFileServer(new FileServerOptions() { FileSystem = contentFileSystem });
         }
     }
-
-    
 }
