b9aab30450a42a9b3560b36271f3300e96c59ce0
Daniel Lo Nigro <daniel@dan.cx>
2015-10-11 00:14:52 -0700

    Fix fresh build and add AppVeyor config

--- dev-build.bat
+++ dev-build.bat
@@ -1,7 +1,7 @@
 @echo off
 IF EXIST "%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" (
-	"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" build.proj /p:BuildType=Dev
+	"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" build.proj
 ) ELSE (
-	"%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe" build.proj /p:BuildType=Dev
+	"%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe" build.proj
 )
 pause
