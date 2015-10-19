613cb37048592bf164c1ba687fd1dc2bc311119a
Kevin Lo <radnor@gmail.com>
2015-10-19 08:43:10 -0700

    Fix OWIN example

--- src/React.Sample.Owin/Content/Sample.jsx
+++ src/React.Sample.Owin/Content/Sample.jsx
@@ -7,7 +7,7 @@
  *  of patent rights can be found in the PATENTS file in the same directory.
  */
 
-CommentsBox = React.createClass({
+var CommentsBox = React.createClass({
 	propTypes: {
 		initialComments: React.PropTypes.array.isRequired
 	},
