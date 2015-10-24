0dbd639efeb1387b1b82850667953e8fd7e441e7
Daniel Lo Nigro <daniel@dan.cx>
2015-10-23 21:34:32 -0700

    Add console app example
    This example shows how ReactJS.NET can be used outside of a web context. References #178

---
+++ src/React.Sample.ConsoleApp/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+ *  Copyright (c) 2015, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+using System;
+
+namespace React.Sample.ConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Initialize();
+
+			ReactSiteConfiguration.Configuration
+				.SetReuseJavaScriptEngines(false)
+				.AddScript("Sample.jsx");
+
+			var environment = React.AssemblyRegistration.Container.Resolve<IReactEnvironment>();
+			var component = environment.CreateComponent("HelloWorld", new { name = "Daniel" });
+			// renderServerOnly omits the data-reactid attributes
+			var html = component.RenderHtml(renderServerOnly: true);
+
+			Console.WriteLine(html);
+			Console.ReadKey();
+		}
+
+		private static void Initialize()
+		{
+			Initializer.Initialize(registration => registration.AsSingleton());
+			var container = React.AssemblyRegistration.Container;
+			// Register some components that are normally provided by the integration library
+			// (eg. React.AspNet or React.Web.Mvc6)
+			container.Register<ICache, NullCache>();
+			container.Register<IFileSystem, SimpleFileSystem>();
+		}
+	}
+}
---
+++ src/React.Sample.ConsoleApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿/*
+ *  Copyright (c) 2015, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("React.Sample.Console")]
+[assembly: AssemblyDescription("Command line sample for ReactJS.NET")]
+[assembly: ComVisible(false)]
+[assembly: Guid("350efb8e-693c-4dfe-8162-c99c359cfc71")]
