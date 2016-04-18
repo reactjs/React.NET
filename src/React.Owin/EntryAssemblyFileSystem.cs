f450bac13a4eb1e643144733ce6e70d01702dad7
Daniel Lo Nigro <daniel@dan.cx>
2016-04-17 19:08:28 -0700

    Fix handling of relative paths in OWIN. References #253.

--- src/React.Owin/EntryAssemblyFileSystem.cs
+++ src/React.Owin/EntryAssemblyFileSystem.cs
@@ -17,10 +17,41 @@ namespace React.Owin
 	/// </summary>
 	internal class EntryAssemblyFileSystem : FileSystemBase
 	{
+		private readonly string _rootPath;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="EntryAssemblyFileSystem"/> class, using the
+		/// entry assembly's location as the root directory.
+		/// </summary>
+		public EntryAssemblyFileSystem()
+			: this(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location))
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="EntryAssemblyFileSystem"/> class, using the 
+		/// specified path as the root directory.
+		/// </summary>
+		/// <param name="rootPath">The root path.</param>
+		public EntryAssemblyFileSystem(string rootPath)
+		{
+			_rootPath = rootPath;
+		}
+
+		/// <summary>
+		/// Converts a path from an application relative path (~/...) to a full filesystem path
+		/// </summary>
+		/// <param name="relativePath">App-relative path of the file</param>
+		/// <returns>Full path of the file</returns>
 		public override string MapPath(string relativePath)
 		{
 			if (relativePath.StartsWith("~"))
-				return Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), relativePath.Replace("~", string.Empty));
+			{
+				relativePath = relativePath
+					.Replace("~/", string.Empty)
+					.Replace('/', '\\');
+				return Path.Combine(_rootPath, relativePath);
+			}
 
 			return relativePath;
 		}
---
+++ src/React.Tests/Owin/EntryAssemblyFileSystemTests.cs
@@ -0,0 +1,27 @@
+﻿/*
+ *  Copyright (c) 2016-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant 
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+using NUnit.Framework;
+using React.Owin;
+
+namespace React.Tests.Owin
+{
+	[TestFixture]
+	public class EntryAssemblyFileSystemTests
+	{
+		[TestCase("C:\\", "~/", "C:\\")]
+		[TestCase("C:\\", "~/foo/bar.js", "C:\\foo\\bar.js")]
+		public void MapPath(string rootPath, string relativePath, string expected)
+		{
+			var fileSystem = new EntryAssemblyFileSystem(rootPath);
+			var result = fileSystem.MapPath(relativePath);
+			Assert.AreEqual(expected, result);
+		}
+	}
+}
