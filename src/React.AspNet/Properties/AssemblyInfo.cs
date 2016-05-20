aa17057a7ace84db8d0e790686cb51dcef1240be
Shiki Byakko <shikigamikun@gmail.com>
2016-05-20 11:35:18 +0900

    Replaced xproj with csproj for React.AspNet

---
+++ src/React.AspNet/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("React.AspNet")]
+[assembly: AssemblyDescription("ReactJS and Babel tools for ASP.NET Core, including ASP.NET Core MVC")]
+[assembly: ComVisible(false)]
+[assembly: Guid("83cbbab4-b6be-4395-ac61-048813e5ac1a")]
