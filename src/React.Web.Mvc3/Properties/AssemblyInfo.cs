956530a3a7188b2042cd2c4e2c6d4383b5a61f63
Daniel Lo Nigro <daniel@dan.cx>
2016-10-01 16:16:58 -0700

    Remove MVC 3 support

--- src/React.Web.Mvc3/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("React.Mvc3")]
-[assembly: AssemblyDescription("ASP.NET MVC 3 extensions for ReactJS.NET")]
-[assembly: ComVisible(false)]
-[assembly: Guid("CEAC08B2-5370-4DC6-82DA-5E9B8CD897A9")]
