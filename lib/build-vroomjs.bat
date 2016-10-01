949150cc4b88b00fa9dcf8f1056823f99e538327
Daniel Lo Nigro <daniel@dan.cx>
2016-10-01 15:00:30 -0700

    Switch to @pauldotknopf's VroomJs fork rather than maintaining our own. Closes #317

--- lib/build-vroomjs.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-"%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe" VroomJs\VroomJs\VroomJs.csproj /p:BuildProjectReferences=false /p:Configuration=Release /p:OutputPath=..\..\ /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=..\..\..\src\Key.snk
-pause
