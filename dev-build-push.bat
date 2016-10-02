6287ea4148d5410f9e9c51e8160170b013490ce7
Daniel Lo Nigro <daniel@dan.cx>
2016-10-01 19:35:21 -0700

    Update build script to handle xproj projects

--- dev-build-push.bat
+++ dev-build-push.bat
@@ -1,7 +1,3 @@
 @echo off
-IF EXIST "%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" (
-	"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" build.proj /t:Package;Push /p:BuildType=Dev
-) ELSE (
-	"%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe" build.proj /t:Package;Push /p:BuildType=Dev
-)
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" build.proj /t:Package;Push /p:BuildType=Dev
 pause
