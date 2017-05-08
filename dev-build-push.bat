347fb527dabfbe828f6621c8221d6343957daa38
Daniel Lo Nigro <daniel@dan.cx>
2017-05-07 19:48:57 -0700

    [WIP] Upgrade to Visual Studio 2017 + csproj tooling

--- dev-build-push.bat
+++ dev-build-push.bat
@@ -1,3 +1,3 @@
 @echo off
-"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" build.proj /t:Package;Push /p:BuildType=Dev
+"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" build.proj /t:Package;Push /p:BuildType=Dev
 pause
