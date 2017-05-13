debe9b98d19d899fdea8031155624a2fdbd25f5a
Gunnar Már Óttarsson <gunni@starcraft.is>
2017-05-13 01:16:41 +0000

    spaces -> tabs

--- src/React.Router/ReactRouterComponent.cs
+++ src/React.Router/ReactRouterComponent.cs
@@ -8,100 +8,114 @@ namespace React.Router
 	/// Represents a React Router JavaScript component.
 	/// </summary>
 	public class ReactRouterComponent : ReactComponent
-    {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ReactRouterComponent"/> class.
-        /// </summary>
-        /// <param name="environment">The environment.</param>
-        /// <param name="configuration">Site-wide configuration.</param>
-        /// <param name="componentName">Name of the component.</param>
-        /// <param name="containerId">The ID of the container DIV for this component</param>
-        public ReactRouterComponent(
-			IReactEnvironment environment, 
-			IReactSiteConfiguration configuration, 
-			string componentName, 
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ReactRouterComponent"/> class.
+		/// </summary>
+		/// <param name="environment">The environment.</param>
+		/// <param name="configuration">Site-wide configuration.</param>
+		/// <param name="componentName">Name of the component.</param>
+		/// <param name="containerId">The ID of the container DIV for this component</param>
+		public ReactRouterComponent(
+			IReactEnvironment environment,
+			IReactSiteConfiguration configuration,
+			string componentName,
 			string containerId
 		) : base(environment, configuration, componentName, containerId)
-        {
+		{
 
-        }
+		}
 
-        /// <summary>
-        /// Render a React StaticRouter Component with context object.
-        /// </summary>
-        /// <param name="path">Current request URL path. F.x. Request.URL.AbsolutePath</param>
-        /// <param name="renderContainerOnly">Only renders component container. Used for client-side only rendering.</param>
-        /// <param name="renderServerOnly">Only renders the common HTML mark up and not any React specific data attributes. Used for server-side only rendering.</param>
-        /// <returns>Object containing HTML in string format and the React Router context object</returns>
-        public ExecutionResult RenderRouterWithContext(string path, bool renderContainerOnly = false, bool renderServerOnly = false)
-        {
-            if (!_configuration.UseServerSideRendering)
-            {
-                renderContainerOnly = true;
-            }
+		/// <summary>
+		/// <see cref="ReactRouterComponent"/>'s do not use this method.
+		/// Instead use RenderRouterWithContext.
+		/// </summary>
+		/// <param name="renderContainerOnly"></param>
+		/// <param name="renderServerOnly"></param>
+		/// <returns></returns>
+		public override string RenderHtml(bool renderContainerOnly = false, bool renderServerOnly = false)
+		{
+			throw new ReactRouterException
+				(@"React Router Components are rendered with RenderRouterWithContext. 
+					Please use ReactComponent if this functionality is not desired.");
+		}
 
-            if (!renderContainerOnly)
-            {
-                EnsureComponentExists();
-            }
+		/// <summary>
+		/// Render a React StaticRouter Component with context object.
+		/// </summary>
+		/// <param name="path">Current request URL path. F.x. Request.Path</param>
+		/// <param name="renderContainerOnly">Only renders component container. Used for client-side only rendering. Does not make sense in this context but included for consistency</param>
+		/// <param name="renderServerOnly">Only renders the common HTML mark up and not any React specific data attributes. Used for server-side only rendering.</param>
+		/// <returns>Object containing HTML in string format and the React Router context object</returns>
+		public ExecutionResult RenderRouterWithContext(string path, bool renderContainerOnly = false, bool renderServerOnly = false)
+		{
+			if (!_configuration.UseServerSideRendering)
+			{
+				renderContainerOnly = true;
+			}
 
-            try
-            {
-                ExecutionResult executionResult;
+			if (!renderContainerOnly)
+			{
+				EnsureComponentExists();
+			}
 
-                if (!renderContainerOnly)
-                {
-                    var componentInitialiser = string.Format("React.createElement({0}, {{ path: '{1}', context: context }})", ComponentName, path );
+			try
+			{
+				ExecutionResult executionResult;
 
-                    var reactDomServerMethod = renderServerOnly ? "renderToStaticMarkup" : "renderToString";
+				if (!renderContainerOnly)
+				{
+					var componentInitialiser = string.Format("React.createElement({0}, {{ path: '{1}', context: context }})", ComponentName, path );
 
-                    var reactRenderCommand = string.Format(@"
+					var reactDomServerMethod = renderServerOnly ? "renderToStaticMarkup" : "renderToString";
 
-                        var context = {{}};
+					var reactRenderCommand = string.Format(@"
 
-                        var renderResult = ReactDOMServer.{0}(
-                            React.createElement({1}, {{ path: '{2}', context: context }})
-                        );
+						var context = {{}};
 
-                        JSON.stringify({{
-                            renderResult: renderResult,
-                            context: context
-                        }});
-                    ", reactDomServerMethod, ComponentName, path);
+						var renderResult = ReactDOMServer.{0}(
+							React.createElement({1}, {{ path: '{2}', context: context }})
+						);
 
-                    var strResult = _environment.Execute<string>(reactRenderCommand);
-                    executionResult = JsonConvert.DeserializeObject<ExecutionResult>(strResult);
-                }
-                else
-                {
-                    executionResult = new ExecutionResult();
-                }
+						JSON.stringify({{
+							renderResult: renderResult,
+							context: context
+						}});
+					", reactDomServerMethod, ComponentName, path);
 
-                string attributes = string.Format("id=\"{0}\"", ContainerId);
-                if (!string.IsNullOrEmpty(ContainerClass))
-                {
-                    attributes += string.Format(" class=\"{0}\"", ContainerClass);
-                }
+					var strResult = _environment.Execute<string>(reactRenderCommand);
+					executionResult = JsonConvert.DeserializeObject<ExecutionResult>(strResult);
+				}
+				else
+				{
+					executionResult = new ExecutionResult();
+				}
 
-                executionResult.renderResult =
-                    string.Format(
-                        "<{2} {0}>{1}</{2}>",
-                        attributes,
-                        executionResult.renderResult,
-                        ContainerTag
-                    );
+				string attributes = string.Format("id=\"{0}\"", ContainerId);
+				if (!string.IsNullOrEmpty(ContainerClass))
+				{
+					attributes += string.Format(" class=\"{0}\"", ContainerClass);
+				}
 
-                return executionResult;
-            }
-            catch (JsRuntimeException ex)
-            {
-                throw new ReactServerRenderingException(string.Format(
-                    "Error while rendering \"{0}\" to \"{2}\": {1}",
-                    ComponentName,
-                    ex.Message,
-                    ContainerId
-                ));
-            }
-        }
-    }
+				executionResult.renderResult =
+					string.Format(
+						"<{2} {0}>{1}</{2}>",
+						attributes,
+						executionResult.renderResult,
+						ContainerTag
+					);
+
+				return executionResult;
+			}
+			catch (JsRuntimeException ex)
+			{
+				throw new ReactServerRenderingException(string.Format(
+					"Error while rendering \"{0}\" to \"{2}\": {1}",
+					ComponentName,
+					ex.Message,
+					ContainerId
+				));
+			}
+		}
+	}
 }
