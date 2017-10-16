0bd9e36a8ba8951c6496ca9f5e1f89c0a5687e08
Dustin Masters <ceo@dustinsoftware.com>
2017-10-15 21:43:45 -0700

    Update to React 16

--- src/React.Core/Resources/react.js
+++ src/React.Core/Resources/react.js
@@ -3,13 +3,14 @@
  *  All rights reserved.
  *
  *  This source code is licensed under the BSD-style license found in the
- *  LICENSE file in the root directory of this source tree. An additional grant 
+ *  LICENSE file in the root directory of this source tree. An additional grant
  *  of patent rights can be found in the PATENTS file in the same directory.
  */
 
 // Exports all the parts of React that ReactJS.NET cares about.
 module.exports = {
-	React: require('react/lib/ReactWithAddons'),
-	ReactDOM: require('react/lib/ReactDOM'),
-	ReactDOMServer: require('react/lib/ReactDOMServer')
+	React: require('react'),
+	ReactDOM: require('react-dom'),
+	ReactDOMServer: require('react-dom/server'),
+	PropTypes: require('prop-types'),
 };
