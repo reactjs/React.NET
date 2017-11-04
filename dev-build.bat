afac9d6cb766106ec7166af36e08b3cf060ceab2
Josh Goldberg <joshuakgoldberg@outlook.com>
2017-11-04 13:00:25 -0700

    Allowed Community, Enterprise, or Professional VS 2017 versions (#457)
    Loops over the potential MSBuild.exe paths for all three, and runs with the first one. If one isn't found it complaints.

--- dev-build.bat
+++ dev-build.bat
@@ -1,3 +1,19 @@
 @echo off
-"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" build.proj
+
+for %%s in (
+    "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
+    "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
+    "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe"
+) do (
+    if exist %%s (
+        echo %%s build.proj
+        %%s build.proj
+        goto :done
+    )
+)
+
+:notfound
+echo Could not find MSBuild.exe. Make sure Visual Studio 2017 is installed and try again.
+
+:done
 pause
