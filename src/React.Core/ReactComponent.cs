13c99266fca73bf5c1dc9d594aa6a76b4c125bd1
Dustin Masters <ceo@dustinsoftware.com>
2017-12-19 07:58:50 -0800

    Document exceptionHandler arguments

--- src/React.Core/ReactComponent.cs
+++ src/React.Core/ReactComponent.cs
@@ -107,7 +107,7 @@ namespace React
 		/// </summary>
 		/// <param name="renderContainerOnly">Only renders component container. Used for client-side only rendering.</param>
 		/// <param name="renderServerOnly">Only renders the common HTML mark up and not any React specific data attributes. Used for server-side only rendering.</param>
-		/// <param name="exceptionHandler"></param>
+		/// <param name="exceptionHandler">A custom exception handler that will be called if a component throws during a render. Args: (Exception ex, string componentName, string containerId)</param>
 		/// <returns>HTML</returns>
 		public virtual string RenderHtml(bool renderContainerOnly = false, bool renderServerOnly = false, Action<Exception, string, string> exceptionHandler = null)
 		{
