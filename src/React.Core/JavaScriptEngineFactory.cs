5e30265c1dd4604d27fafbcd5afc62a1a34fe209
Josh Goldberg <joshuakgoldberg@outlook.com>
2018-07-11 23:50:24 -0700

    Added missing Oxford comma to EnsureREactLoaded complaint (#570)
    :)

--- src/React.Core/JavaScriptEngineFactory.cs
+++ src/React.Core/JavaScriptEngineFactory.cs
@@ -185,7 +185,7 @@ namespace React
 			{
 				throw new ReactNotInitialisedException(
 					"React has not been loaded correctly. Please expose your version of React as global " +
-					"variables named 'React', 'ReactDOM' and 'ReactDOMServer', or enable the " +
+					"variables named 'React', 'ReactDOM', and 'ReactDOMServer', or enable the " +
 					"'LoadReact' configuration option to use the built-in version of React."
 				);
 			}
