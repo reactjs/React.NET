76fd500a35b6b9d8ef7ad74d60f0fafc7918fe63
Dustin Masters <ceo@dustinsoftware.com>
2018-08-12 21:30:18 -0700

    Fix Cassette sample build

---
+++ src/React.Sample.Cassette/App_Start/ReactConfig.cs
@@ -0,0 +1,29 @@
+/*
+ *  Copyright (c) 2014-Present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant 
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+using JavaScriptEngineSwitcher.Core;
+using JavaScriptEngineSwitcher.Msie;
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(React.Sample.Mvc4.ReactConfig), "Configure")]
+
+namespace React.Sample.Mvc4
+{
+	public static class ReactConfig
+	{
+		public static void Configure()
+		{
+			ReactSiteConfiguration.Configuration
+				.SetReuseJavaScriptEngines(true)
+				.AddScript("~/Content/Sample.jsx");
+
+			JsEngineSwitcher.Current.DefaultEngineName = MsieJsEngine.EngineName;
+			JsEngineSwitcher.Current.EngineFactories.AddMsie();
+		}
+	}
+}
