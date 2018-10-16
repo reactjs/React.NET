4042e10fa976d226dbc2f76af9ebdbf567df2908
Stefan Saroiu <ssaroiu@microsoft.com>
2018-10-16 09:21:31 -0700

    Added all three VS 2017 versions to dev-build-push.bat, release-build-push.bat, and release-build.bat

--- dev-build-push.bat
+++ dev-build-push.bat
@@ -1,3 +1,19 @@
 @echo off
-"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" build.proj /t:Package;Push /p:BuildType=Dev
+
+for %%s in (
+    "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
+    "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
+    "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe"
+) do (
+    if exist %%s (
+        echo %%s build.proj /t:Package;Push /p:BuildType=Dev
+        %%s build.proj /t:Package;Push /p:BuildType=Dev
+        goto :done
+    )
+)
+
+:notfound
+echo Could not find MSBuild.exe. Make sure Visual Studio 2017 is installed and try again.
+
+:done
 pause
