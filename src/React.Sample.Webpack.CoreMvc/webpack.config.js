a44865ebb6d1505b39454f3ac0f0f66045d5edd0
Dustin Masters <ceo@dustinsoftware.com>
2019-02-13 00:12:11 -0500

    Add vendor bundle to webpack sample (#722)

--- src/React.Sample.Webpack.CoreMvc/webpack.config.js
+++ src/React.Sample.Webpack.CoreMvc/webpack.config.js
@@ -1,15 +1,36 @@
+const path = require('path');
+
 module.exports = {
-	entry: './Content/components/expose-components.js',
+	entry: {
+		components: './Content/components/expose-components.js',
+	},
 	output: {
-		filename: './wwwroot/components-bundle.generated.js'
+		filename: '[name].js',
+		globalObject: 'this',
+		path: path.resolve(__dirname, 'wwwroot/dist'),
+	},
+	mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+	optimization: {
+		runtimeChunk: {
+			name: 'runtime', // necessary when using multiple entrypoints on the same page
+		},
+		splitChunks: {
+			cacheGroups: {
+				commons: {
+					test: /[\\/]node_modules[\\/]/,
+					name: 'vendor',
+					chunks: 'all',
+				},
+			},
+		},
 	},
 	module: {
 		rules: [
 			{
 				test: /\.jsx?$/,
 				exclude: /node_modules/,
-				loader: 'babel-loader'
-			}
-		]
-	}
+				loader: 'babel-loader',
+			},
+		],
+	},
 };
